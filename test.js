0e225921d7d43bed8e02a89baa8107990f725872
andrew morton <drewish@katherinehouse.com>
2013-11-03 22:41:11 -0800

    Starting some tests. Better late than never.

---
+++ test.js
@@ -0,0 +1,39 @@
+require('should');
+var Logo = require('./logo');
+
+describe('Logo', function () {
+  var logo = new Logo();
+
+  describe('#tokenizeInput', function () {
+    it('should create token objects', function() {
+      var tokens = logo.tokenizeInput('plus 3 5');
+      tokens.should.be.an.instanceOf(Array).and.have.length(3);
+    });
+  });
+
+  describe('commands', function() {
+    describe('make', function() {
+      it('declares variables', function() {
+        logo.runInput('make "bar 23');
+        logo.variables.BAR.should.equal(23);
+      });
+    });
+    describe('sum', function() {
+      it('adds numbers', function() {
+        logo.runInput('make "baz sum 1 3');
+        logo.variables.BAZ.should.equal(4);
+      });
+      it('adds variable to number', function() {
+        logo.runInput('make "foo 3 make "baz sum :foo 5');
+        logo.variables.BAZ.should.equal(8);
+      });
+    });
+    describe('repeat', function() {
+      it('runs a block multiple times', function() {
+        logo.runInput('make "foo 0 repeat 3 [ make "foo sum :foo 1 ]');
+        logo.variables.FOO.should.equal(3);
+      });
+    });
+  });
+
+});
